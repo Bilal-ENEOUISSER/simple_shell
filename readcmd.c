2a4fcc4559d31f758f7fec44a820e8de88ae0b82
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2023-12-05 21:17:00 +0100

    Update readcmd.c

--- readcmd.c
+++ readcmd.c
@@ -4,20 +4,22 @@
  * read_cmd - gets the next line of input from STDIN
  * Return: cmd
  */
-
 char *read_cmd(void)
 {
-	char *cmd = NULL;
-	size_t length = 0;
-	ssize_t j;
-	if(isatty(STDIN_FILENO)
-		)
-	write(STDOUT_FILENO, "$ ", 2);
-	j = getline(&cmd, &length, stdin);
-	if (j == -1)
-	{
-		free(cmd);
-		return (NULL);
-	}
-	return (cmd);
+    char *cmd = NULL;
+    size_t length = 0;
+    ssize_t j;
+
+    if (isatty(STDIN_FILENO))
+        write(STDOUT_FILENO, "$ ", 2);
+
+    j = getline(&cmd, &length, stdin);
+
+    if (j == -1)
+    {
+        free(cmd);
+        return (NULL);
+    }
+
+    return (cmd);
 }
