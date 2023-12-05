50458b9472f1eae2c322f449c27cf331cd841d40
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2023-12-05 22:00:41 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,26 +7,33 @@
  * Return: 0 (Always Success)
  */
 
+
+char **env;  // Definition of the env variable
+
 int main(int ac, char **av)
 {
-	char *string = NULL, **cmd = NULL;
-	int status = 0;
-	(void)ac;
+    char *string = NULL, **cmd = NULL;
+    int status = 0;
+    (void)ac;
 
-	while (1)
+    env = environ;
 
-	{
-		string = read_cmd();
-		if (string == NULL)
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			return (status);
-		}
-		cmd = split(string);
-		if (!cmd)
-			continue;
-	}
-	status = _execute(cmd, av);
-	return (0);
+    while (1)
+    {
+        string = read_cmd();
+        if (string == NULL)
+        {
+            if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 1);
+            return (status);
+        }
+        cmd = split(string);
+        if (!cmd)
+            continue;
+
+        status = _execute(cmd, av);
+    }
+
+    return 0;
 }
+
