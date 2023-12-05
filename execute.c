e08778f87a14fac7aebbe94c41c95b87ca0e0a0d
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2023-12-05 18:59:35 +0100

    Create execute.c

---
+++ execute.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+/**
+ * _execute - execute the command
+ * @cmd: command
+ * @av: args
+ * Return:
+ */
+
+int _execute(char **cmd, char **av)
+{
+	pid_t child;
+	int status;
+
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(cmd[0], cmd, env) == -1)
+		{
+			perror(av[0]);
+			free_arr(cmd);
+			exit(0);
+		}
+	}
+	else
+	{
+		waitpid(child, &status, 0);
+		free_arr(cmd);
+	}
+	return (WEXITSTATUS(status));
+}
