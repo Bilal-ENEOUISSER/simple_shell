b7c1522d5789c000f57f5a0a1eeb2ebe1de81fd6
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2023-12-05 18:34:45 +0100

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,37 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH '\0'
+#define DELIM " \t\n"
+extern char **env;
+
+char *read_cmd(void);
+char **split(char *string);
+int _execute(char **cmd, char **av);
+void free_arr(char **arr);
+
+/* toem_string.c */
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+
+/* toem_string1.c */
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+
+#endif
