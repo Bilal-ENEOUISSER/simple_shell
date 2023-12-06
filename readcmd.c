7181b6f371af5c1f81d5a2ba18d00b294acb7964
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2023-12-06 16:12:18 +0100

    Update readcmd.c

--- readcmd.c
+++ readcmd.c
@@ -11,11 +11,8 @@ char *read_cmd(void)
 	size_t length = 0;
 	ssize_t j;
 
-	if (isatty(STDIN_FILENO))<<<<<< HEAD
-	write(STDOUT_FILENO, "$ ", 2);
-=======
+	if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, "$ ", 2);
->>>>>>> 99cf62e983dd47d0e829d25f4ac8265b5d160527
 	j = getline(&cmd, &length, stdin);
 	if (j == -1)
 	{
