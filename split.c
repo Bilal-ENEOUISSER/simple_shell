a08af967e4de291183e54644f3b246600498f3de
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2023-12-06 15:51:52 +0100

    Update split.c

--- split.c
+++ split.c
@@ -7,46 +7,39 @@
 
 char **split(char *string)
 {
-    char *token = NULL, *tmp = NULL;
-    char **cmd = NULL;
-    int n = 0, i = 0;
+	char *token = NULL, *tmp = NULL;
+	char **cmd = NULL;
+	int n = 0, i = 0;
 
-    if (!string)
-        return (NULL);
-
-    tmp = _strdup(string);
-    token = strtok(tmp, DELIM);
-
-    if (token == NULL)
-    {
-        free(tmp), tmp = NULL;
-        free(string), string = NULL;
-        return (NULL);
-    }
-
-    while (token)
-    {
-        n++;
-        token = strtok(NULL, DELIM);
-    }
-
-    free(tmp), tmp = NULL;
-    cmd = malloc(sizeof(char *) * (n + 1));
-
-    if (!cmd)
-    {
-        free(string), string = NULL;
-        return (NULL);
-    }
-
-    while (token)
-    {
-        cmd[i] = _strdup(token);
-        token = strtok(NULL, DELIM);
-        i++;
-    }
-
-    free(string), string = NULL;
-    cmd[i] = NULL;
-    return (cmd);
+	if (!string)
+		return (NULL);
+	tmp = _strdup(string);
+	token = strtok(tmp, DELIM);
+	if (token == NULL)
+	{
+		free(tmp), tmp = NULL;
+		free(string), string = NULL;
+		return (NULL);
+	}
+	while (token)
+	{
+		n++;
+		token = strtok(NULL, DELIM);
+	}
+	free(tmp), tmp = NULL;
+	cmd = malloc(sizeof(char *) * (n + 1));
+	if (!cmd)
+	{
+		free(string), string = NULL;
+		return (NULL);
+	}
+	while (token)
+	{
+	  cmd[i] = _strdup(token);
+	  token = strtok(NULL, DELIM);
+	  i++;
+	}
+	free(string), string = NULL;
+	cmd[i] = NULL;
+	return (cmd);
 }
